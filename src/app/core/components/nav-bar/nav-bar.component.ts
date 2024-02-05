a3f88c509dd4431e4e43c4d786f2cb63ccf3a0df
brenogomesf <bgferreira44@gmail.com>
2024-02-05 18:49:57 -0300

    feat: clear input

--- src/app/core/components/nav-bar/nav-bar.component.ts
+++ src/app/core/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from '../../services/cookie.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -12,6 +13,7 @@ export class NavBarComponent implements OnInit {
   session : boolean = false;
   constructor(
     private cookieService: CookieService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -20,5 +22,10 @@ export class NavBarComponent implements OnInit {
   viewSession(){
     const res = this.cookieService.getCookie('session')
     this.session = res ? true : false;
+  }
+
+  clearCookie() {
+    this.cookieService.deleteCookie('session');
+    this.router.navigate(['/']);
   }
 }
